b696cd3c6a85d150f7dd5a4575a5654493d387e9
fujitargz <61151276+fujitargz@users.noreply.github.com>
2022-06-08 21:01:40 +0900

    コンポーネントのドキュメント作成 #22

--- src/components/MonthlyCalendar/DayCell/index.tsx
+++ src/components/MonthlyCalendar/DayCell/index.tsx
@@ -2,8 +2,15 @@ import type { ReactNode } from "react";
 
 import styles from "./index.module.scss";
 export interface DayCellProps {
+  /**
+   * マスに表示する内容
+   */
   children: ReactNode;
 }
+
+/**
+ * DayCell は MonthlyCalendar の各日を表示するマスです。
+ */
 export const DayCell = ({ children }: DayCellProps) => {
   return <td className={styles["day-cell"]}>{children}</td>;
 };
