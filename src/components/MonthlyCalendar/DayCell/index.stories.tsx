e4daad6c7e4d74714d763151fd9e7dc243d74a56
fujitargz <61151276+fujitargz@users.noreply.github.com>
2022-07-02 13:30:58 +0900

    MonthlyCalendar: 前後の月の日付を薄く #32

--- src/components/MonthlyCalendar/DayCell/index.stories.tsx
+++ src/components/MonthlyCalendar/DayCell/index.stories.tsx
@@ -4,8 +4,50 @@ import { DayCell } from "./DayCell";
 
 export default {
   component: DayCell,
+  argTypes: {
+    date: {
+      control: {
+        type: "number",
+        min: 1,
+        max: 31,
+      },
+    },
+    today: {
+      control: {
+        type: "boolean",
+      },
+    },
+    currentMonth: {
+      control: {
+        type: "boolean",
+      },
+    },
+  },
 } as ComponentMeta<typeof DayCell>;
 
-export const dayCell: ComponentStory<typeof DayCell> = () => (
-  <DayCell>1</DayCell>
-);
+const Template: ComponentStory<typeof DayCell> = ({
+  date,
+  today,
+  currentMonth,
+}) => <DayCell date={date} today={today} currentMonth={currentMonth} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  date: 26,
+  today: false,
+  currentMonth: true,
+};
+
+export const Today = Template.bind({});
+Today.args = {
+  date: 26,
+  today: true,
+  currentMonth: true,
+};
+
+export const NotCurrentMonth = Template.bind({});
+NotCurrentMonth.args = {
+  date: 26,
+  today: false,
+  currentMonth: false,
+};
